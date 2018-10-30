b5bcc739d3437910a243627b8e0766368ff211aa
gcclgw <505737211@qq.com>
2018-10-30 23:19:13 +0800

    消费者框架1.0

---
+++ src/main/java/com/jk/SpringbootClientApplication.java
@@ -0,0 +1,14 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource(value = {"classpath:spring-dubbo-consumer.xml"})
+public class SpringbootClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootClientApplication.class, args);
+    }
+}
