6163c65db392ab693afd7fffa99dea2683766c14
wyz <940417479@qq.com>
2018-11-06 10:55:25 +0800

    wyz adver

---
+++ src/main/java/com/jk/service/business/BussinessService.java
@@ -0,0 +1,14 @@
+package com.jk.service.business;
+
+
+import com.jk.model.business.Business;
+
+import java.util.List;
+
+
+public interface BussinessService {
+    List<Business> queryBusiness();
+    void deleteBus(Integer qid);
+    void addBus(Business business);
+
+}
