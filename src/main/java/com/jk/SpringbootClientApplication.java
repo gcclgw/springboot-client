e5b4d39c94c6b5f88c73a6a686fb801d4d5e5bbe
gcclgw <505737211@qq.com>
2018-11-08 15:09:31 +0800

    11.8

--- src/main/java/com/jk/SpringbootClientApplication.java
+++ src/main/java/com/jk/SpringbootClientApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.amqp.core.Queue;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
